1fb1b22b93297a92d47a9df18af80fda480e062f
IPBUF <service@ipbuf.com>
2022-06-16 22:14:23 +0800

    Update ipbuf.py

--- ipbuf.py
+++ ipbuf.py
@@ -20,7 +20,7 @@ def GetHtml(url):
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:93.0) Gecko/20100101 Firefox/93.0',
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
                 'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
-                'Accept-Encoding': 'gzip, deflate, br',
+                'Accept-Encoding': 'gzip, deflate',
                 'Cookie': 'CookieID=%s; UserName=%s' % (CookieID, UserName),
                 'Connection': 'keep-alive'
                 }
